333ab270bb99bf8401a27cad29309449ed94a061
bstriner <bstriner@gmail.com>
2018-01-25 23:08:02 -0500

    pytorch-example

--- recitation-2/pytorch-example.py
+++ recitation-2/pytorch-example.py
@@ -14,12 +14,11 @@ class OnesCounter(torch.nn.Module):
         self.to_hidden2 = torch.nn.Linear(2 * input_size, 2 * input_size)
         self.hidden_sigmoid2 = torch.nn.Sigmoid()
         self.to_binary = torch.nn.Linear(2 * input_size, count_bitwidth)
-        self.binary_sigmoid = torch.nn.Sigmoid()
 
     def forward(self, input_val):
         hidden1 = self.hidden_sigmoid1(self.to_hidden1(input_val))
         hidden2 = self.hidden_sigmoid2(self.to_hidden2(hidden1))
-        return self.binary_sigmoid(self.to_binary(hidden2))
+        return self.to_binary(hidden2)
 
 
 def load_data():
@@ -35,10 +34,8 @@ def load_data():
     # Create the binary encoding of the ground truth labels
     # As a bit of practice using Numpy, we're going to do this
     # without using a Python loop.
-    labels_binary = np.zeros((10000, 3))
-    labels_binary[:, 2] = (labels % 2 >= 1)
-    labels_binary[:, 1] = (labels % 4 >= 2)
-    labels_binary[:, 0] = (labels % 8 >= 4)
+    labels_binary = np.unpackbits(labels.astype(np.uint8)).reshape((-1,8))
+    labels_binary = labels_binary[:,-3:]
 
     return (data, labels_binary)
 
@@ -60,7 +57,7 @@ def training_routine(num_epochs, minibatch_size, learn_rate):
     (data, labels_binary) = load_data()
 
     my_net = OnesCounter(7)  # Create the network,
-    loss_fn = torch.nn.MSELoss()  # and choose the loss function / optimizer
+    loss_fn = torch.nn.BCEWithLogitsLoss()  # and choose the loss function / optimizer
     optim = torch.optim.SGD(my_net.parameters(), lr=learn_rate)
 
     if torch.cuda.is_available():
@@ -73,14 +70,23 @@ def training_routine(num_epochs, minibatch_size, learn_rate):
     data_loader = torch.utils.data.DataLoader(
         dataset, batch_size=minibatch_size, shuffle=True)
 
-    for _ in range(num_epochs):
+    for epoch in range(num_epochs):
+        losses = []
         for (input_val, label) in data_loader:
             optim.zero_grad()  # Reset the gradients
 
             prediction = my_net(to_variable(input_val))  # Feed forward
             loss = loss_fn(prediction, to_variable(label))  # Compute losses
             loss.backward()  # Backpropagate the gradients
-
+            losses.append(loss.data.cpu().numpy())
             optim.step()  # Update the network
-
+        print("Epoch {} Loss: {:.4f}".format(epoch, np.asscalar(np.mean(losses))))
     return my_net
+
+if __name__ == '__main__':
+    net = training_routine(500, 64, 0.1)
+    x = to_variable(to_tensor(np.array([[1,0,1,1,0,1,0], [0,1,0,0,0,0,0], [1,1,1,0,0,0,0]])))
+    y = net(x)
+    print('X: {}'.format(x.data.cpu().numpy()))
+    print('Y (logits): {}'.format(y.data.cpu().numpy()))
+    print('Y (argmax): {}'.format(y.data.cpu().numpy() > 0))
